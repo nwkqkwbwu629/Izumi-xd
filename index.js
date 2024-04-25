77a8f0953aa9e8e0e920ff8f6f982eb43d7d08f9
Eypz Satânico <105191630+sataniceypz@users.noreply.github.com>
2024-04-25 11:54:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,19 @@ fs.readdirSync("./lib/database/").forEach((plugin) => {
     require("./lib/database/" + plugin);
   }
 });
+const express = require('express');
+const app = express();
+const port = 3000;
 
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
 async function Abhiy() {
   console.log("Syncing Database");
   await config.DATABASE.sync();
